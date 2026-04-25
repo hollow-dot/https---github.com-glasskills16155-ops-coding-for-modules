efe9985510b4228af229372221da4c500ea9fbe7
glasskills16155-ops <glasskills16155@gmail.com>
2026-04-25 18:22:43 +0100

    re formating my HTML web pages

--- working web site/app1.py
+++ working web site/app1.py
@@ -4,14 +4,30 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def login():
     return render_template("login.html")
 
-@app.route("/home.html") 
-def home_page():
-    return render_template("/home.html")
+@app.route("/post_page.html") 
+def post_page():
+    return render_template("/post_page.html")
 
-@app.route("/generate", methods=["GET", "POST"])
+@app.route("/home_page.html") 
+def home_page():
+    return render_template("/home_page.html")
+
+@app.route("/create_an_account.html") 
+def create_acount():
+    return render_template("/create_an_account.html")
+
+@app.route("/edit_and_delete_post.html") 
+def edit_delete():
+    return render_template("/edit_and_delete_post.html")
+
+@app.route("/delete_post.html") 
+def delete():
+    return render_template("/edit_and_delete_post.html")
+
+@app.route("/post-page", methods=["GET", "POST"])
 def generate():
     if request.method == "POST":
         username = request.form.get('fozi_b3Ar')
@@ -19,11 +35,27 @@ def generate():
         if not username:
             return "Invalid username", 400 # this is error for empty box
 
-        if username not in ["fozi_b3Ar", "Go4zo"]:
+        if username not in ["fozi_b3Ar"]:
             return render_template("login.html"), 401
         
 
-        return render_template("home_page.html")
+        return render_template("post_page.html")
+@app.route("/password", methods=["GET", "POST"])
+
+@app.route("/password", methods=["GET", "POST"])
+def password():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    password_code = request.form.get('Gonz0')
+
+    if not password_code:
+        return "Invalid password", 400
+
+    if password_code != "Gonz0":
+        return render_template("login.html"), 401
+
+    return render_template("post_page.html")
         
 
 UPLOAD_FOLDER = r"C:\Users\mengw\OneDrive\coding\working web site\templates\folder_for_images"
